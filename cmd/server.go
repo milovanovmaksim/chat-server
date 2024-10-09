9317c4d931e6545e42100b5af3e8427f24f7a108
milovanovmaksim <milovanov160386@gmail.com>
2024-10-09 15:39:40 +0300

    implemented Delete, SendMessage methods

--- cmd/server.go
+++ cmd/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	desc "github.com/olezhek28/microservices_course_boilerplate/pkg/chat_v1"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // Server ...
@@ -16,4 +17,16 @@ type server struct {
 func (s *server) Create(_ context.Context, req *desc.CreateChatRequest) (*desc.CreateChatResponse, error) {
 	log.Printf("Create new chat with users: %v", req.Usernames)
 	return &desc.CreateChatResponse{Id: 1}, nil
+}
+
+// Delete ...
+func (s *server) Delete(_ context.Context, req *desc.DeleteChatRequest) (*emptypb.Empty, error) {
+	log.Printf("Delete chat with id: %d", req.GetId())
+	return &emptypb.Empty{}, nil
+}
+
+// SendMessage ...
+func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Send message with content: %v", req.Message)
+	return &emptypb.Empty{}, nil
 }
