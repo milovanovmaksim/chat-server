efe28fbbca4884523040e2ad942b1d92227c3745
milovanovmaksim <milovanov160386@gmail.com>
2024-10-09 16:04:46 +0300

    implemented main func

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/olezhek28/microservices_course_boilerplate/cmd/server"
+	desc "github.com/olezhek28/microservices_course_boilerplate/pkg/chat_v1"
 )
 
+const grpcPort = 50051
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatServer(s, &server.Server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
