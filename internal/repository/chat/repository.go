7c6f2e9be793025dd03428a7dfad0de8f40fd841
milovanovmaksim <milovanov160386@gmail.com>
2024-11-11 16:11:24 +0300

    добавил метод NewChatRepository

--- internal/repository/chat/repository.go
+++ internal/repository/chat/repository.go
@@ -1,6 +1,15 @@
 package chat
 
+import (
+	"github.com/milovanovmaksim/chat-server/internal/client/database"
+	"github.com/milovanovmaksim/chat-server/internal/repository"
+)
 
 type chatRepository struct {
+	db database.Client
+}
 
+// NewChatRepository создает новый объект, удовлетворяющий интерфейсу repository.ChatRepository.
+func NewChatRepository(db database.Client) repository.ChatRepository {
+	return &chatRepository{db}
 }
