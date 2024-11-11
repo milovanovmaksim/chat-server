cc49663c457fcd831ab1e3361d2399f9b6857ace
milovanovmaksim <milovanov160386@gmail.com>
2024-11-11 17:15:47 +0300

    добавил метод CreateChat

---
+++ internal/repository/chat/create.go
@@ -0,0 +1,12 @@
+package chat
+
+import (
+	"context"
+
+	"github.com/milovanovmaksim/chat-server/internal/repository"
+)
+
+
+func (c *chatRepositoryImpl) CreateChat(ctx context.Context, request repository.CreateChatRequest) (repository.CreateChatResponse, error) {
+	
+}
