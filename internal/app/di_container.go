7649a1056f4b996d626343f6dff237e43c8a6b9a
milovanovmaksim <milovanov160386@gmail.com>
2024-11-12 20:28:11 +0300

    добавил метод ChatService

--- internal/app/di_container.go
+++ internal/app/di_container.go
@@ -6,11 +6,14 @@ import (
 
 	"github.com/milovanovmaksim/chat-server/internal/client/database"
 	"github.com/milovanovmaksim/chat-server/internal/client/database/postgresql"
+	"github.com/milovanovmaksim/chat-server/internal/client/database/transaction"
 	"github.com/milovanovmaksim/chat-server/internal/closer"
 	"github.com/milovanovmaksim/chat-server/internal/repository"
-	"github.com/milovanovmaksim/chat-server/internal/repository/chat"
+	chatRepo "github.com/milovanovmaksim/chat-server/internal/repository/chat"
 	"github.com/milovanovmaksim/chat-server/internal/server"
+	"github.com/milovanovmaksim/chat-server/internal/server/grpc"
 	"github.com/milovanovmaksim/chat-server/internal/service"
+	"github.com/milovanovmaksim/chat-server/internal/service/chat"
 )
 
 type diContainer struct {
@@ -29,10 +32,19 @@ func newDiContainer() diContainer {
 // ChatRepository возвращает объект, удовлетворяющий интерфейсу repository.ChatRepository
 func (di *diContainer) ChatRepository(ctx context.Context) repository.ChatRepository {
 	if di.chatRepository == nil {
-		di.chatRepository = chat.NewChatRepository(di.DBClient(ctx))
+		di.chatRepository = chatRepo.NewChatRepository(di.DBClient(ctx))
 	}
 
 	return di.chatRepository
+}
+
+// ChatService возврашщает объект, удовлетворяющий интерфейсу service.ChatService.
+func (di *diContainer) ChatService(ctx context.Context) service.ChatService {
+	if di.chatService == nil {
+		di.chatService = chat.NewChatService(di.ChatRepository(ctx), di.TxManager(ctx))
+	}
+
+	return di.chatService
 }
 
 // DBClient возвращает объект, удовлетвoряющий интерфейсу database.Client.
@@ -67,4 +79,27 @@ func (di *diContainer) DBConfig() database.DBConfig {
 	}
 
 	return di.pgConfig
+}
+
+// GRPCConfig возвращает объект, удовлетворяющий интерфейсу server.ServerConfig.
+func (di *diContainer) GRPCConfig() server.ServerConfig {
+	if di.grpcConfig == nil {
+		cfg, err := grpc.NewGrpcConfigFromEnv()
+		if err != nil {
+			log.Fatalf("failed to get grpc config || error: %v", err.Error())
+		}
+
+		di.grpcConfig = cfg
+	}
+
+	return di.grpcConfig
+}
+
+// TxManager возвращает объект, удовлетвoряющий интерфейсу database.TxManager.
+func (di *diContainer) TxManager(ctx context.Context) database.TxManager {
+	if di.txManager == nil {
+		di.txManager = transaction.NewTransactionManager(di.DBClient(ctx).DB())
+	}
+
+	return di.txManager
 }
