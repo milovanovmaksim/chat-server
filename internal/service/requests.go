e76817e467f0d86ebe8bc1b2a928eb2ce7d452b4
milovanovmaksim <milovanov160386@gmail.com>
2024-11-12 14:12:46 +0300

    добавил requests.go

---
+++ internal/service/requests.go
@@ -0,0 +1,12 @@
+package service
+
+// CreateChatRequest запрос на создание нового чата.
+type CreateChatRequest struct {
+	TitleChat string
+	UserIds   []int64
+}
+
+// DeleteChatRequest запрос на удаление чата.
+type DeleteChatRequest struct {
+	Id int64
+}
