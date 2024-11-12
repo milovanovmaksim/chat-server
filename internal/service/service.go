5bcfd4a5f87c69d82b1973e7074dc39e6dfff213
milovanovmaksim <milovanov160386@gmail.com>
2024-11-12 14:13:31 +0300

    добавил интерфейс ChatService

---
+++ internal/service/service.go
@@ -0,0 +1,8 @@
+package service
+
+import "context"
+
+type ChatService interface {
+	CreateChat(ctx context.Context, request CreateChatRequest) (CreateChatResponse, error)
+	DeleteChat(ctx context.Context, request DeleteChatRequest) error
+}
