6058bc70471d8f5385a5a71486900c095f86aff3
milovanovmaksim <milovanov160386@gmail.com>
2024-11-12 14:37:35 +0300

    имплементация метода DeleteChat

---
+++ internal/service/chat/delete.go
@@ -0,0 +1,20 @@
+package chat
+
+import (
+	"context"
+	"log"
+
+	"github.com/milovanovmaksim/chat-server/internal/repository"
+	"github.com/milovanovmaksim/chat-server/internal/service"
+)
+
+// DeleteChat удаляет чат.
+func (c *chatServiceImpl) DeleteChat(ctx context.Context, request service.DeleteChatRequest) error {
+	err := c.chatRepository.DeleteCaht(ctx, repository.DeleteChatRequest{Id: request.Id})
+	if err != nil {
+		log.Printf("failed to delete chat || error: %v", err)
+		return err
+	}
+
+	return nil
+}
