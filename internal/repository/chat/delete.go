37873b89a8ab79b6f6acf64c97930ba7576c6aea
milovanovmaksim <milovanov160386@gmail.com>
2024-11-12 14:01:01 +0300

    имплементация DeleteCaht

---
+++ internal/repository/chat/delete.go
@@ -0,0 +1,22 @@
+package chat
+
+import (
+	"context"
+	"log"
+
+	"github.com/milovanovmaksim/chat-server/internal/client/database"
+	"github.com/milovanovmaksim/chat-server/internal/repository"
+)
+
+// DeleteCaht удаляет чат из БД.
+func (c *chatRepositoryImpl) DeleteCaht(ctx context.Context, request repository.DeleteChatRequest) error {
+	query := database.Query{Name: "Delete chat", QueryRaw: "DELETE FROM CHATS WHERE id = $1"}
+
+	_, err := c.db.DB().ExecContext(ctx, query, request.Id)
+	if err != nil {
+		log.Printf("failed to delete user || error: %v", err)
+		return err
+	}
+
+	return nil
+}
