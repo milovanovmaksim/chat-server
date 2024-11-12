f88a8df16a717475c6b87c0c7321eb1d974634b6
milovanovmaksim <milovanov160386@gmail.com>
2024-11-12 16:12:12 +0300

    изменил возвращаемый тип данных

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,9 +2,7 @@ package repository
 
 import "context"
 
-
-
 type ChatRepository interface {
-	CreateChat(ctx context.Context, request CreateChatRequest) (CreateChatResponse, error)
+	CreateChat(ctx context.Context, request CreateChatRequest) (*CreateChatResponse, error)
 	DeleteCaht(ctx context.Context, request DeleteChatRequest) error
 }
