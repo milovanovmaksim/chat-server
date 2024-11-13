4198fb372439ee65d89c8090dfc36edd387066b0
milovanovmaksim <milovanov160386@gmail.com>
2024-11-13 16:27:15 +0300

    удалил ненужный код

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,13 @@ import (
 	"context"
 	"log"
 
-	"github.com/joho/godotenv"
-
 	"github.com/milovanovmaksim/chat-server/internal/app"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("failed to load config || err: %v", err)
-	}
-
 	ctx := context.Background()
 
-	app, err := app.NewApp(ctx)
+	app, err := app.NewApp(ctx, ".env")
 	if err != nil {
 		log.Fatalf("failed to init app: %s", err.Error())
 	}
