5b6b0250561f574818a447cfc666c044bba0ce72
milovanovmaksim <milovanov160386@gmail.com>
2024-11-13 13:35:19 +0300

    изменил тип поля таблицы базы данных

--- migrations/20241019155022_create_chat.sql
+++ migrations/20241019155022_create_chat.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE chats (
   id serial PRIMARY KEY,
-  title varchar(64) not null unique,
+  title TEXT not null unique,
   user_ids bigint [],
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP
 );
 
 -- +goose Down
-DROP TABLE users;
+DROP TABLE chats;
