9cc081d06a4915390a168da3dc58cb748fae2437
milovanovmaksim <milovanov160386@gmail.com>
2024-11-13 14:17:55 +0300

    удалил поле

--- migrations/20241019155022_create_chat.sql
+++ migrations/20241019155022_create_chat.sql
@@ -2,7 +2,6 @@
 CREATE TABLE chats (
   id serial PRIMARY KEY,
   title TEXT not null unique,
-  user_ids bigint [],
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP
 );
