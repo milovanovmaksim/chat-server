0fc838000293f5ef804b518e2742132d2d716ace
milovanovmaksim <milovanov160386@gmail.com>
2024-11-15 10:46:27 +0300

    добавил строку документации для ChatService

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import "context"
 
+// ChatService интерфейс, отвечающий за бизнес логику приложения.
 type ChatService interface {
 	CreateChat(ctx context.Context, request CreateChatRequest) (*CreateChatResponse, error)
 	DeleteChat(ctx context.Context, request DeleteChatRequest) error
