3c41651ad73172eb7e1f4f1db087ad39b1337d60
milovanovmaksim <milovanov160386@gmail.com>
2024-11-15 10:53:42 +0300

    добавил строку документации

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import "context"
 
+// ChatRepository интерфейс, определяющий набор методов CRUD для работы с БД.
 type ChatRepository interface {
 	CreateChat(ctx context.Context, request CreateChatRequest) (*CreateChatResponse, error)
 	DeleteCaht(ctx context.Context, request DeleteChatRequest) error
