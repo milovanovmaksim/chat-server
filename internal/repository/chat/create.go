c0e041d78c47774851ffdb567dee993d2d3b4327
milovanovmaksim <milovanov160386@gmail.com>
2024-11-17 23:48:37 +0300

    передал ссылку в метод

--- internal/repository/chat/create.go
+++ internal/repository/chat/create.go
@@ -12,8 +12,8 @@ import (
 func (c *chatRepositoryImpl) CreateChat(ctx context.Context, request repository.CreateChatRequest) (*repository.CreateChatResponse, error) {
 	var response repository.CreateChatResponse
 
-	query := database.Query{Name: "Create chat", QueryRaw: "INSERT INTO chats (title, user_ids) VALUES($1, $2) RETURNING id"}
-	err := c.db.DB().ScanOneContext(ctx, response, query, request.TitleChat, request.UserIDs)
+	query := database.Query{Name: "Create chat", QueryRaw: "INSERT INTO chats (title) VALUES($1) RETURNING id"}
+	err := c.db.DB().ScanOneContext(ctx, &response, query, request.TitleChat)
 	if err != nil {
 		log.Printf("failed to insert user || err: %v", err)
 		return nil, err
