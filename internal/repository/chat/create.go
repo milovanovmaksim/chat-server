528fd80758dea8bb4985113be14d494cbc2db208
milovanovmaksim <milovanov160386@gmail.com>
2024-11-18 13:18:13 +0300

    добавил метод CreateChatUser

--- internal/repository/chat/create.go
+++ internal/repository/chat/create.go
@@ -8,16 +8,32 @@ import (
 	"github.com/milovanovmaksim/chat-server/internal/repository"
 )
 
-// CreateChat создает новый чат, сохраняет информацию о чате в БД.
+// CreateChat создает новый чат в БД.
 func (c *chatRepositoryImpl) CreateChat(ctx context.Context, request repository.CreateChatRequest) (*repository.CreateChatResponse, error) {
 	var response repository.CreateChatResponse
 
 	query := database.Query{Name: "Create chat", QueryRaw: "INSERT INTO chats (title) VALUES($1) RETURNING id"}
+
 	err := c.db.DB().ScanOneContext(ctx, &response, query, request.TitleChat)
 	if err != nil {
-		log.Printf("failed to insert user || err: %v", err)
+		log.Printf("failed to insert chat || err: %v", err)
 		return nil, err
 	}
 
 	return &response, nil
+}
+
+// CreateChatUser создает запись в таблицу "user_chats".
+func (c *chatRepositoryImpl) CreateChatUser(ctx context.Context, user_id int64, chat_id int64) (int64, error) {
+	var id int64
+
+	query := database.Query{Name: "Create chat user", QueryRaw: "INSERT INTO user_chats (user_id, chat_id) VALUES($1, $2) RETURNING id"}
+
+	err := c.db.DB().ScanOneContext(ctx, &id, query, user_id, chat_id)
+	if err != nil {
+		log.Printf("failed to create chat_user || error: %v", err)
+		return 0, err
+	}
+
+	return id, nil
 }
