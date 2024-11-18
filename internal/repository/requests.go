757c2a68663ad6efd361b4fb080ab0ecc5b4c27f
milovanovmaksim <milovanov160386@gmail.com>
2024-11-18 14:07:49 +0300

    изменил строки документации

--- internal/repository/requests.go
+++ internal/repository/requests.go
@@ -10,12 +10,12 @@ type DeleteChatRequest struct {
 	ID int64
 }
 
-// CreateUserRequest запрос на создание нового пользователя чатом.
+// CreateUserRequest запрос на создание нового пользователя в БД.
 type CreateUserRequest struct {
 	UserID int64
 }
 
-// DeleteUserRequest запрос на удаление пользователя из чата.
+// DeleteUserRequest запрос на удаление пользователя из БД.
 type DeleteUserRequest struct {
 	UserID int64
 }
