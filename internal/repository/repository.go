8b2ca8288d90a5abafb16d37a978e79e031d3ca2
milovanovmaksim <milovanov160386@gmail.com>
2024-11-18 13:14:12 +0300

    добавил методы в интерфейсы

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,14 +2,16 @@ package repository
 
 import "context"
 
-// ChatRepository интерфейс, определяющий набор методов CRUD для работы с БД.
+// ChatRepository интерфейс, определяющий набор методов для работы с таблицей "chats" базы данных.
 type ChatRepository interface {
 	CreateChat(ctx context.Context, request CreateChatRequest) (*CreateChatResponse, error)
 	DeleteCaht(ctx context.Context, request DeleteChatRequest) error
+	CreateChatUser(ctx context.Context, user_id int64, chat_id int64) (int64, error)
 }
 
 // UserRepository интерфейс, определяющий набор методов для работы с таблицей "users" базы данных.
 type UserRepository interface {
 	CreateUser(ctx context.Context, request CreateUserRequest) (*CreateUserResponse, error)
 	DeleteUser(ctx context.Context, request DeleteUserRequest) error
+	UserExists(ctx context.Context, request int64) (bool, error)
 }
