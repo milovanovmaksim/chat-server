0697ec47c827a735ded1d568c1143708a7f73727
milovanovmaksim <milovanov160386@gmail.com>
2024-11-19 13:23:30 +0300

    имплементация интерфейса Into для CreateChatRequest

--- internal/service/requests.go
+++ internal/service/requests.go
@@ -1,9 +1,29 @@
 package service
 
+import (
+	"database/sql"
+
+	"github.com/milovanovmaksim/chat-server/internal/repository"
+)
+
 // CreateChatRequest запрос на создание нового чата.
 type CreateChatRequest struct {
 	TitleChat string
 	UserIDs   []int64
+}
+
+func (c CreateChatRequest) Into() repository.CreateChatRequest {
+	var titleChat sql.NullString
+
+	if c.TitleChat == "" {
+		titleChat = sql.NullString{String: "", Valid: false}
+	} else {
+		titleChat = sql.NullString{String: c.TitleChat, Valid: true}
+	}
+
+	return repository.CreateChatRequest{
+		TitleChat: titleChat,
+	}
 }
 
 // DeleteChatRequest запрос на удаление чата.
