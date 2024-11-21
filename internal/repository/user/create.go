567d2d59488e30976735a99c78db482bf8d0ca47
milovanovmaksim <milovanov160386@gmail.com>
2024-11-21 22:27:01 +0300

    добавил проверку на ошибку

--- internal/repository/user/create.go
+++ internal/repository/user/create.go
@@ -12,9 +12,13 @@ import (
 func (u *userRepositoryImpl) CreateUser(ctx context.Context, request repository.CreateUserRequest) (*repository.CreateUserResponse, error) {
 	var response repository.CreateUserResponse
 
-	query := database.Query{Name: "Create user", QueryRaw: "INSERT INTO users (user_id) VALUES($1) RETURNING user_id"}
+	query := database.Query{Name: "Create user", QueryRaw: "INSERT INTO users (user_id) VALUES($1) ON CONFLICT DO NOTHING RETURNING user_id"}
 	err := u.db.DB().ScanOneContext(ctx, &response, query, request.UserID)
 	if err != nil {
+		if err.Error() == "no rows in result set" {
+			response.ID = request.UserID
+			return &response, nil
+		}
 		log.Printf("failed to insert user || err: %v", err)
 		return nil, err
 	}
