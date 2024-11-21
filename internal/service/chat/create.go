9eedf45a5663507c4d1132e8e04142fe2d88b534
milovanovmaksim <milovanov160386@gmail.com>
2024-11-21 16:45:17 +0300

    синхронная реализация

--- internal/service/chat/create.go
+++ internal/service/chat/create.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"log"
-	"sync"
 
 	"github.com/milovanovmaksim/chat-server/internal/repository"
 	"github.com/milovanovmaksim/chat-server/internal/service"
@@ -13,6 +12,7 @@ import (
 // CreateChat создает новый чат.
 func (c *chatServiceImpl) CreateChat(ctx context.Context, request service.CreateChatRequest) (*service.CreateChatResponse, error) {
 	var chat *repository.CreateChatResponse
+	var user *repository.CreateUserResponse
 
 	if len(request.UserIDs) == 0 {
 		return nil, errors.New("user_ids is empty")
@@ -27,36 +27,19 @@ func (c *chatServiceImpl) CreateChat(ctx context.Context, request service.Create
 			return errTx
 		}
 
-		wg := sync.WaitGroup{}
-
 		for _, userID := range request.UserIDs {
-			wg.Add(1)
-			go func() error {
-				defer wg.Done()
+			user, errTx = c.userRepository.CreateUser(ctx, repository.CreateUserRequest{UserID: userID})
+			if errTx != nil {
+				log.Printf("failed to create new chat || error: %v", errTx)
+				return errTx
+			}
 
-				ok, errTx := c.userRepository.UserExists(ctx, userID)
-				if errTx != nil {
-					log.Printf("failed to get user || error: %v", errTx)
-					return errTx
-				}
-
-				if !ok {
-					_, errTx = c.userRepository.CreateUser(ctx, repository.CreateUserRequest{UserID: userID})
-					if errTx != nil {
-						log.Printf("failed to create new chat || error: %v", errTx)
-						return errTx
-					}
-				}
-
-				_, errTx = c.chatRepository.CreateChatUser(ctx, userID, chat.ID)
-				if errTx != nil {
-					log.Printf("failed to create new chat || error: %v", errTx)
-					return errTx
-				}
-				return nil
-			}()
+			_, errTx = c.chatRepository.CreateChatUser(ctx, user.ID, chat.ID)
+			if errTx != nil {
+				log.Printf("failed to create new chat || error: %v", errTx)
+				return errTx
+			}
 		}
-		wg.Wait()
 
 		return nil
 	})
