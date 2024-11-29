43286223510aaa9f40e83c361b75a7a90aab1641
milovanovmaksim <milovanov160386@gmail.com>
2024-11-29 13:48:02 +0300

    поменял типы в контракте

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,14 +4,14 @@ import "context"
 
 // ChatRepository интерфейс, определяющий набор методов для работы с таблицей "chats" базы данных.
 type ChatRepository interface {
-	CreateChat(ctx context.Context, request CreateChatRequest) (*CreateChatResponse, error)
-	DeleteCaht(ctx context.Context, request DeleteChatRequest) error
+	CreateChat(ctx context.Context, chatTitle string) (int64, error)
+	DeleteCaht(ctx context.Context, request int64) error
 	CreateChatUser(ctx context.Context, userID int64, chatID int64) (int64, error)
 }
 
 // UserRepository интерфейс, определяющий набор методов для работы с таблицей "users" базы данных.
 type UserRepository interface {
-	CreateUser(ctx context.Context, request CreateUserRequest) (*CreateUserResponse, error)
-	DeleteUser(ctx context.Context, request DeleteUserRequest) error
+	CreateUser(ctx context.Context, request int64) (int64, error)
+	DeleteUser(ctx context.Context, request int64) error
 	UserExists(ctx context.Context, request int64) (bool, error)
 }
